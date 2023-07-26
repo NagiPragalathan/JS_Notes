d75cd22775bd32f60cb8e2bc6e0bb79a25b328f8
nagipragalathan <kk2069037@gmail.com>
2023-07-26 14:00:36 +0530

    Keyword, operators & variables are completed

---
+++ variables/operator.js
@@ -0,0 +1,15 @@
+// A good practice is to put spaces around operators ( = + - * / ):
+
+let x = y + z;
+
+// JavaScript Line Length and Line Breaks
+
+/*
+-----------------------------------------------------------
+Note :
+-----------------------------------------------------------
+JavaScript Line Length and Line Breaks
+For best readability, programmers often like to avoid code lines longer than 80 characters.
+
+If a JavaScript statement does not fit on one line, the best place to break it is after an operator:
+*/
---
+++ variables/sample.py
@@ -0,0 +1,6 @@
+hello=10;
+# python not allowed the $ symbol !!
+
+# a-b=10 
+#CLDR Short Name: beaming face with smiling eyes
+print("\U0001F601")
